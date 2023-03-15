35c9e22650cbec6ebf4bf3d1b5d74bce72657f49
홍혁준 <jjj8337721@naver.com>
2023-03-15 21:39:15 +0900

    init

---
+++ src/main/java/kr/co/hhjpetclinic/HhjPetclinicApplication.java
@@ -0,0 +1,13 @@
+package kr.co.hhjpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhjPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhjPetclinicApplication.class, args);
+    }
+
+}
